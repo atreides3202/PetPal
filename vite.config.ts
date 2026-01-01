ec8e28b9da882e8097cd36b39edc1f03269feb9c
atreides3202 <lynn3202@gmail.com>
2026-01-01 22:37:17 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PetPal/', // This fixes the blank white screen
+  base: './', // Using './' instead of '/PetPal/' is a "magic" fix for many users
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
   }
 })
