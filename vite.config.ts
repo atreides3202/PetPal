ef5998355cad085a06107c4d6fceeec6418e74f1
atreides3202 <lynn3202@gmail.com>
2026-01-01 22:47:46 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Using './' instead of '/PetPal/' is a "magic" fix for many users
+  base: './', 
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
   }
 })
